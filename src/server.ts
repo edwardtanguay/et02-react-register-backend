63f8a3961e50132a742f7349fbff592c30f8357e
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-22 11:16:52 +0200

    works

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,24 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3049;
+
 interface IUser {
-    firstName: string;
-    lastName: string;
-    accessGroups: string[];
+	firstName: string;
+	lastName: string;
+	accessGroups: string[];
 }
 
 const user: IUser = {
-    firstName: "Hendrick",
-    lastName: "Denzmann",
-    accessGroups: ['loggedInUsers', 'members']
+	firstName: "Hendrick",
+	lastName: "Denzmann",
+	accessGroups: ['loggedInUsers', 'members']
 };
 
-console.log(user);
+app.get('/', (req: express.Request, res: express.Response) => {
+	res.send(user);
+});
+
+app.listen(PORT, () => {
+	console.log(`listening to API on http://localhost:${PORT}`);
+});
