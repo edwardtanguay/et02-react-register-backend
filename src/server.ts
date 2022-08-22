4c6f68b1e2c86c6f6b6c959553eda5a8d4ffbeb3
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-22 11:35:11 +0200

    ready for heroku

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = 3049;
+const PORT = process.env.PORT || 3049;
 
 interface IUser {
 	firstName: string;
